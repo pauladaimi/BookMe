ef65624ee7910e92b0e12579bd63b0ec80a0a796
Karim <Karim@github.com>
2018-04-10 17:35:52 +0300

    ok
    zesrb

--- app/src/main/java/com/example/user/myapplication/Chair.java
+++ app/src/main/java/com/example/user/myapplication/Chair.java
@@ -1,5 +1,19 @@
 package com.example.user.myapplication;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
 public class Chair extends Spot{
-	
+    private Rect rectangle;
+
+    public void draw(Canvas canvas)  {
+        Paint paint = new Paint();
+        paint.setColor(Color.GREEN);
+        canvas.drawRect(rectangle, paint);
+    }
+
+
+
 }
