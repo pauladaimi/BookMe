1774b8bd4983f22f0ef40e7b7cef7c7657ac455c
Paul <pma03@mail.aub.edu>
2018-04-10 18:36:40 +0300

    new
    new

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.user.myapplication;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button search;
+
     private TextView mTextMessage;
+    private ImageView imageView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,20 +33,39 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText("Search");
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText("Profile");
+                    //mTextMessage.setText("Profile");
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText("Settings");
+                    setContentView(R.layout.profile_acitivity);
                     return true;
             }
             return false;
         }
     };
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=(ImageView) findViewById(R.id.imageView);
+        search=(Button) findViewById(R.id.searchBtn);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bitmap bitmap = Bitmap.createBitmap(500,300,Bitmap.Config.ARGB_8888);
+
+                Canvas canvas = new Canvas(bitmap);
+
+                canvas.drawColor(Color.BLUE);
+                Rect rect = new Rect(50,50,canvas.getWidth()-50,canvas.getHeight()-50);
+                Paint paint = new Paint();
+                paint.setColor(Color.GREEN);
+
+                canvas.drawRect(rect,paint);
+                imageView.setImageBitmap(bitmap);
+            }
+        });
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
