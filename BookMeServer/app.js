07bc95e8d51106cb29f10c0b883a978bae74751f
Didanny <37907412+Didanny@users.noreply.github.com>
2018-04-11 19:01:12 +0300

    Initialize dependencies

---
+++ BookMeServer/app.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    expressSanitizer = require("express-sanitizer"),
+    methodOverride = require("method-override");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
+
+app.get("/", function(req, res) {
+    res.render("home.ejs");
+});
+
+app.listen(8000, function() {
+   console.log("BookMe Server Running..."); 
+});
