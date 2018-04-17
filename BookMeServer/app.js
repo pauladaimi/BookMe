25f887b49c51385634230f4d20762368f798d693
Didanny <37907412+Didanny@users.noreply.github.com>
2018-04-17 19:21:35 +0300

    .

--- BookMeServer/app.js
+++ BookMeServer/app.js
@@ -13,6 +13,10 @@ app.get("/", function(req, res) {
     res.render("home.ejs");
 });
 
+app.get("/spaces/new", function(req, res) {
+	res.render("canvas.ejs");
+});
+
 app.listen(8000, function() {
    console.log("BookMe Server Running..."); 
 });
